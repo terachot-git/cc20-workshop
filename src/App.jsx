df7661ed73d9549bef8e311a11bcf2d22cb90c70
terachot-git <terachotdev@gmail.com>
2025-05-16 09:23:06 +0700

    start with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+ return (
+   <div className="app">
+     <h1 className="text-3xl ">Codecamp20</h1>
+     <button className="btn btn-primary ">ok</button>
+   </div>
+ )}
+export default App
